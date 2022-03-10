ae59f4b9d823872c991fd44d92587e31a07ce5d3
Nalanihv <nalanihevi@gmail.com>
2022-03-10 11:38:50 -0600

    Clase ingrediente ,completa (getters,setters y contructor)

--- src/Ingrediente.java
+++ src/Ingrediente.java
@@ -1,6 +1,29 @@
+
 public class Ingrediente {
      private String nombre;
    private  double peso;
    private boolean salado;
+//gettesr setters
+    public String getNombre() {return nombre;}
+
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+    public double getPeso() {return peso;}
+
+    public void setPeso(double peso) {this.peso = peso;}
+
+    public boolean isSalado() {return salado;}
+
+    public void setSalado(boolean salado) {this.salado = salado;}
+
+//constructor
+    public Ingrediente(String nombre, double peso, boolean salado) {
+        this.nombre = nombre;
+        this.peso = peso;
+        this.salado = salado;
+        System.out.println("Ingrediente creado");
+    }
+
+
 
 }
