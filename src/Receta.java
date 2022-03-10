97ccee2dc92ae185752cefe704507b498a63e234
Nalanihv <nalanihevi@gmail.com>
2022-03-10 11:58:13 -0600

    Clase receta

--- src/Receta.java
+++ src/Receta.java
@@ -1,3 +1,47 @@
+import java.util.ArrayList;
+
 public class Receta {
+    //array list, puede crecer o decrecer a placer,porporciona metodos(.add)
     //ingrediente [], pasos[],tiempoPreparacion double
+    private double tiempoPreparacion;
+    private ArrayList <Ingrediente> ingredientes;
+    private ArrayList <String> pasos;
+
+    public Receta(double tiempoPreparacion, ArrayList<Ingrediente> ingredientes, ArrayList<String> pasos) {
+        this.tiempoPreparacion = tiempoPreparacion;
+        this.ingredientes = ingredientes;
+        this.pasos = pasos;
+        System.out.println("Receta creada");
+    }
+
+    public double getTiempoPreparacion() {
+        return tiempoPreparacion;}
+
+    public void setTiempoPreparacion(double tiempoPreparacion) {
+        this.tiempoPreparacion = tiempoPreparacion;}
+
+    public ArrayList<Ingrediente> getIngredientes() {
+        return ingredientes;}
+
+    public void setIngredientes(ArrayList<Ingrediente> ingredientes) {
+        this.ingredientes = ingredientes;}
+
+    public ArrayList<String> getPasos() {
+        return pasos;}
+
+    public void setPasos(ArrayList<String> pasos) {
+        this.pasos = pasos;}
+
+
+    public void mostrarIngredientes(){
+        //tipo de dato, nombre y de donde
+        for (Ingrediente ingredente:ingredientes) {
+            System.out.println(ingredente);
+        }
+
+        for (int i=0; i<ingredientes.size();i++){
+            System.out.println(ingredientes.get(i));
+        }
+    }
+
 }
