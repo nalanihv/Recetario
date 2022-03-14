8223549f8200a438296cc819c7ccd248fe4ea1e7
Nalanihv <nalanihevi@gmail.com>
2022-03-14 11:49:49 -0600

    clase recetario

--- src/Recetario.java
+++ src/Recetario.java
@@ -1,4 +1,46 @@
+import java.util.ArrayList;
+
 public class Recetario {
-    String autor, titulo;
+
+     private String autor;
+     private String titulo;
+     private ArrayList<Receta> recetas;
+    //private ArrayList <Ingrediente> ingredientes;
     //recetas arreglo
+
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public ArrayList<Receta> getRecetas() {
+        return recetas;
+    }
+
+    public void setRecetas(ArrayList<Receta> recetas) {
+        this.recetas = recetas;
+    }
+
+    public Recetario(String autor, String titulo, ArrayList<Receta> recetas) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.recetas = recetas;
+    }
+
+    public  void mostrarRecetario (){
+        System.out.println(titulo);
+        System.out.println(autor);
+    }
 }
